9d905123d4cca4b762bc8d7a0c5d4d42d028e8d6
waclaw_pawel <waclaw.pawel@wp.pl>
2024-06-13 22:13:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,39 +2,22 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
+import generuj_grupy as grupa
 
 """
-# Welcome to Streamlit!
+# Witaj Sprzedawco Allegro!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+Wrzucaj tu swoje statystyki aby dowiedzieć się więcej o ofertach
 
-In the meantime, below is an example of what you can do with just a few lines of code:
 """
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+file = st.file_uploader("ZAŁADUJ Statystyki oferty z panelu ads np. statystyki_oferty_01-03-2024_31-03-2024.xlsx")
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+######kod generujący statsy#####
+if file:
+    g1, g2 = grupa.generuj(file)
+    """Grupa 1 promocji"""
+    st.write(g1)
+    """Grupa 2 Oferty do poprawy"""
+    st.write(g2)
+    
